b39b5fa42262c6eb2da08fcd503584cf64eccf4d
tonykung <tonykung06@gmail.com>
2015-07-12 13:57:57 +0800

    check file size plugin created

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,22 +30,19 @@ module.exports = function(grunt) {
 
     // Configuration to be run (and then tested).
     my_grunt_plugin: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
+      options: {
+        isDebug: true,
+        foldersToScan: ['./test/']
       }
+      // dev: {
+      //   files: []
+      // },
+      // prod: {
+      //   options: {
+      //       isDebug: false
+      //   },
+      //   files: []
+      // }
     },
 
     // Unit tests.
--- tasks/my_grunt_plugin.js
+++ tasks/my_grunt_plugin.js
@@ -1,50 +1,64 @@
 /*
- * grunt-my-grunt-plugin
- * https://github.com/Administrator/my-grunt-plugin
- *
- * Copyright (c) 2015 tonykung
- * Licensed under the MIT license.
- */
+* grunt-my-grunt-plugin
+* https://github.com/Administrator/my-grunt-plugin
+*
+* Copyright (c) 2015 tonykung
+* Licensed under the MIT license.
+*/
 
 'use strict';
 
+
+var fs = require('fs');
+
+
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('my_grunt_plugin', 'The best Grunt plugin ever.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+    function showDebugInfo (options) {
+        if (options.isDebug) {
+            grunt.log.writeflags(options, 'Custom Options');
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+    }
 
-      // Handle options.
-      src += options.punctuation;
+    function verifyFoldersExist (folderPaths) {
+        if (folderPaths.length < 1) {
+            grunt.fail.fatal("no folder is specified in the option 'foldersToScan'");
+        }
+        folderPaths.forEach(function (path) {
+            if (path === '' || path === undefined || !grunt.file.exists(path) || !grunt.file.isDir(path)) {
+                grunt.fail.fatal("any one of the paths specidied in foldersToScan is not correct, not exist? not a directory path?");
+            }
+        });
+    }
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+    function checkFileSize (folders) {
+        folders.forEach(function (folderToScan){
+            grunt.log.writeln('Peeking %s', folderToScan);
+            grunt.file.recurse(folderToScan, function (absolutePath, rootDir, subDir, fileName){
+                var stats, asBytes;
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+                if (grunt.file.isFile(absolutePath)) {
+                    stats = fs.statSync(absolutePath);
+                    asBytes = stats.size / 1024;
+                    grunt.log.writeln('Found file %s with size of %s kb', fileName, asBytes);
+                }
+            });
+            grunt.log.writeln();
+        })
+    }
+
+    grunt.registerTask('my_grunt_plugin', 'The best Grunt plugin ever.', function() {
+        var options = this.options({
+            foldersToScan: [],
+            isDebug: false
+        });
+
+        showDebugInfo(options);
+        verifyFoldersExist(options.foldersToScan);
+        checkFileSize(options.foldersToScan);
     });
-  });
 
 };
